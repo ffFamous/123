da8e2af43204f87d8fbacaf983fa033801bd0be7
ffFamous <ingvar.kokorin@gmail.com>
2020-12-27 20:15:49 +0300

    Initial commit

---
+++ src/Hello.java
@@ -0,0 +1,15 @@
+public class Hello {
+    public static void main(String[] args) {
+        int mouse; // число мышек
+        int weight; // вес кота в граммах
+        mouse = 0;
+        weight = 4500;
+
+        if (mouse != 0 && weight / mouse < 1000) {
+            System.out.println("Можно кормить кота");
+        }
+    }
+}
+
+
+
---
+++ src/Yellos.java
@@ -0,0 +1,15 @@
+public class Yellos {
+
+        int a;
+        int b;
+
+        public static void main(String[] args)
+        {
+            System.out.print("2");
+        }
+
+        public static double pi()
+        {
+            return 3.14;
+        }
+    }
