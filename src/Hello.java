999295aeb070292bbd6273f49c802bc0c0e18e1f
ffFamous <ingvar.kokorin@gmail.com>
2020-12-27 20:50:37 +0300

    Init 1

--- src/Hello.java
+++ src/Hello.java
@@ -2,10 +2,11 @@ public class Hello {
     public static void main(String[] args) {
         int mouse; // число мышек
         int weight; // вес кота в граммах
-        mouse = 0;
+        mouse = 5;
         weight = 4500;
 
         if (mouse != 0 && weight / mouse < 1000) {
+            System.out.println("Можно кормить кота");
             System.out.println("Можно кормить кота");
         }
     }
